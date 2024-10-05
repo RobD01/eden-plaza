214fde54386fc028ab17c09b0025c74f3688d92a
RobD01 <medbz2k3@gmail.com>
2024-10-04 17:40:37 -0700

    remove auth, appwrite, query

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import "./globals.css";
 import { HashRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import QueryProvider from "./lib/react-query/QueryProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <HashRouter>
-    <QueryProvider>
-      <App />
-    </QueryProvider>
+    <App />
   </HashRouter>
 );
